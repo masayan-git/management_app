e9419f2ace4264cfb1b65927d15b7f38dee8642c
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-09-05 13:24:18 +0900

    アクティブストレージ導入

--- app/models/apartment_message.rb
+++ app/models/apartment_message.rb
@@ -2,4 +2,5 @@ class ApartmentMessage < ApplicationRecord
   belongs_to :master, optional: true
   belongs_to :apartment, optional: true
   belongs_to :chatroom_apartment
+  has_one_attached :image
 end
--- app/models/residence_message.rb
+++ app/models/residence_message.rb
@@ -1,5 +1,6 @@
 class ResidenceMessage < ApplicationRecord
-  belongs_to :master
-  belongs_to :residence
+  belongs_to :master, optional: true
+  belongs_to :residence, optional: true
   belongs_to :chatroom_residence
+  has_one_attached :image
 end
