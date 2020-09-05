b214358ec4e80f74b6b2c9087cce50e40b325725
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-09-05 16:02:21 +0900

    画像投稿機能の実装

--- app/models/apartment_message.rb
+++ app/models/apartment_message.rb
@@ -3,4 +3,10 @@ class ApartmentMessage < ApplicationRecord
   belongs_to :apartment, optional: true
   belongs_to :chatroom_apartment
   has_one_attached :image
+  validates :message, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
