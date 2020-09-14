af55c9bb53afe01bbc41afd60d017f9c4bf6f73f
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-09-14 14:06:58 +0900

    seed　記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Master.create!(email:'master@gmail.com',password:'11111111',confirmed_at:Time.now)
+
+Apartment.create!(email:'apartment@gmail.com',password:'11111111',confirmed_at:Time.now)
+
+Residence.create!(email:'residence@gmail.com',password:'11111111',confirmed_at:Time.now)
